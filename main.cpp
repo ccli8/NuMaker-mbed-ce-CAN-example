5dc52c43db8393c1ccb6062367741e8a0384b114
cyliang <devnull@localhost>
2023-03-14 09:53:27 +0000

    Revise the reserved pins of M467 CAN0/CAN1

--- main.cpp
+++ main.cpp
@@ -25,7 +25,8 @@ CAN canObj(D9, D8);         // (rd, td) Change to match external attachment
 #elif defined(TARGET_NUMAKER_IOT_M487) 
 CAN canObj(A0, A1);         // Change to match external attachment
 #elif defined(TARGET_NUMAKER_IOT_M467) 
-CAN canObj(PJ_11, PJ_10);         // (rd, td) Change to match external attachment
+CAN canObj(A0, A1);         // CAN1(rd, td) Change to match external attachment
+//CAN canObj(D9, D8);       // CAN0
 #endif
 
 CANMessage canMsg;
